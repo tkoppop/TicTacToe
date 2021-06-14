08833596f89444cc5e176a853993983dc3319ff8
Lucas Wung <tkoppopwung130@gmail.com>
2021-06-14 14:34:53 -0700

    Update script.js

--- Java Script Version/script.js
+++ Java Script Version/script.js
@@ -16,12 +16,8 @@ const winningMessageElement = document.getElementById("winningMessage")
 const restartButton = document.getElementById("restartButton")
 const winningMessageTextElement = document.querySelector("[data-winning-message-text]")
 let circleTurn
-
 startGame()
-
 restartButton.addEventListener("click", startGame)
-
-
 function startGame() {
   circleTurn = false
   cellElements.forEach(cell => {
@@ -33,7 +29,6 @@ function startGame() {
   setBoardHoverClass()
   winningMessageElement.classList.remove("show")
 }
-
 function handleClick(e) {
   const cell = e.target
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
@@ -47,7 +42,6 @@ function handleClick(e) {
     setBoardHoverClass()
   }
 }
-
 function endGame(draw) {
   if (draw) {
     winningMessageTextElement.innerText = "Draw!"
@@ -56,21 +50,17 @@ function endGame(draw) {
   }
   winningMessageElement.classList.add("show")
 }
-
 function isDraw() {
   return [...cellElements].every(cell => {
     return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
   })
 }
-
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass)
 }
-
 function swapTurns() {
   circleTurn = !circleTurn
 }
-
 function setBoardHoverClass() {
   board.classList.remove(X_CLASS)
   board.classList.remove(CIRCLE_CLASS)
@@ -80,7 +70,6 @@ function setBoardHoverClass() {
     board.classList.add(X_CLASS)
   }
 }
-
 function checkWin(currentClass) {
   return WINNING_COMBINATIONS.some(combination => {
     return combination.every(index => {
